738a07c1aaab33a743d73a4846b5c16aa42a7343
imrajashish <imrajashish07@gmail.com>
2022-02-06 20:45:42 +0530

    Basic Stracture

---
+++ Singly Link List/Basic Structure.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(val){
+        this.data = val;
+        this.null = null;
+    }
+}
+class SinglyLinkList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0; 
+    }
+}
+const ab = new SinglyLinkList()
